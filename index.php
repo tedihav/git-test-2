6092dac8399ee720b569082845165655fce5a234
tedihav <lubtsihav@gmail.com>
2021-04-13 15:33:18 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -11,6 +11,10 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi facere, nam culpa laudantium beatae id assumenda autem animi, maxime eum voluptates quidem aliquam modi cum. Facilis incidunt fugiat accusamus quisquam?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 
 </html>
